9a763475cc2f7b2359de9b865361e6d79921d042
caio <caiocobacho@hotmail.com>
2019-08-19 14:57:28 -0300

    first

---
+++ SeleniumTestForEA/Pages/UserFormPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumTestForEA.Pages
+{
+    class UserFormPage
+    {
+
+
+        private IWebDriver _driver;
+
+        public UserFormPage(IWebDriver driver)
+        {
+            _driver = driver;
+
+        }
+
+        public IWebElement txtInicial => _driver.FindElement(By.Id("Initial"));
+        public IWebElement txtFirstName => _driver.FindElement(By.Id("FirstName"));
+        public IWebElement txtMiddleName => _driver.FindElement(By.Id("MiddleName"));
+        public IWebElement txtLanguageEN => _driver.FindElement(By.Id("english"));
+
+
+        public void EnterUserForm(string initial, string firstName, string middleName, string language) {
+            txtInicial.SendKeys(initial);
+            txtFirstName.SendKeys(firstName);
+            txtMiddleName.SendKeys(middleName);
+            txtLanguageEN.Click();
+        }
+
+
+    }
+}
